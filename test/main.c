47e4e6e214e1d5bd7f918a5fcf234fe19376eff1
marihachi <marihachi0620@gmail.com>
2023-12-02 11:02:17 +0900

    comment

--- test/main.c
+++ test/main.c
@@ -5,12 +5,13 @@
 #define TEST_CASE_NOT_EXISTS 101
 #define REGISTER_TEST(name) if (strcmp(argv[1], #name) == 0) return name()
 
-// list of test case
+// declare test cases
 int test_list(void);
 
 int main(int argc, char* argv[]) {
   if (argc < 2) return TEST_CASE_REQUIRED;
 
+  // register test cases
   REGISTER_TEST(test_list);
 
   return TEST_CASE_NOT_EXISTS;
