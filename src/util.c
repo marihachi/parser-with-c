de63c9fdbc575e631c45318b47b5fa4f1ac681e0
marihachi <marihachi0620@gmail.com>
2023-12-03 10:17:38 +0900

    memory dump

---
+++ src/util.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "util.h"
+
+void mem_dump(void *address) {
+  if (address == NULL) {
+    printf("NULL ptr\n");
+    return;
+  }
+  char *ptr = address;
+  for (int y = 0; y < 4; y++) {
+    printf("%18p: ", (ptr + (y * 16)));
+    for (int x = 0; x < 16; x++) {
+      printf("%02X ", (unsigned char)(*(ptr + (y * 16 + x))));
+    }
+    printf("\n");
+  }
+}
--- src/util.h
+++ src/util.h
@@ -3,4 +3,6 @@
 
 #define THROW(message) do{printf(message);exit(EXIT_FAILURE);}while(0)
 
+void mem_dump(void *address);
+
 #endif
