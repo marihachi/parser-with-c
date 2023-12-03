c37db46bfd1e7ad0227d0918ec41b9115f9c5e7d
marihachi <marihachi0620@gmail.com>
2023-12-03 13:08:10 +0900

    list.h

--- src/list.h
+++ src/list.h
@@ -1,6 +1,33 @@
 #ifndef _LIST_H
 #define _LIST_H
 
+/**
+ * Usage:
+ * ```c
+ * // create list
+ * list_ptr_t list = NULL;
+ *
+ * // add item
+ * list_add(&list, NULL);
+ *
+ * // insert item
+ * list_insert(&list, 0, NULL);
+ *
+ * // get list length
+ * length = list_count(list);
+ *
+ * // get item
+ * item = list_at(list, 0);
+ *
+ * // seek list
+ * while (list != NULL) {
+ *   // Do anything here
+ *   // *list->item
+ *   list_seek_next(&list);
+ * }
+ * ```
+*/
+
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -16,8 +43,8 @@ typedef list_cell_t * list_ptr_t;
 
 void list_add(list_ptr_t *list, void *item);
 void list_insert(list_ptr_t *list, int index, void *item);
+void list_seek_next(list_ptr_t *list);
 void *list_at(list_ptr_t list, int index);
 int list_count(list_ptr_t list);
-void list_seek_next(list_ptr_t *list);
 
 #endif
